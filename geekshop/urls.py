576713db8368f8da7dfdff3efc56b849a8613204
Oleg <didoleg@mail.ru>
2021-11-11 19:29:11 +0700

    Lesson 4. User login and logout

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -11,8 +11,10 @@ urlpatterns = [
     path("", mainapp.main, name="main"),
     path("products/", include("mainapp.urls", namespace="products")),
     path("contact/", mainapp.contact, name="contact"),
+    path("auth/", include("authnapp.urls", namespace="auth")),
 ]
 
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
