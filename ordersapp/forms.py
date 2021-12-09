a562b7dec918a9cf93d66c5f88984e335ae6fa43
Oleg <didoleg@mail.ru>
2021-12-09 19:38:45 +0700

    lesson_12. Dynamic summary

--- ordersapp/forms.py
+++ ordersapp/forms.py
@@ -15,6 +15,8 @@ class OrderForm(forms.ModelForm):
 
 
 class OrderItemForm(forms.ModelForm):
+    price = forms.CharField(label="цена", required=False)
+
     def __init__(self, *args, **kwargs):
         super(OrderItemForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
@@ -23,3 +25,5 @@ class OrderItemForm(forms.ModelForm):
     class Meta:
         model = OrderItem
         exclude = ()
+
+        
